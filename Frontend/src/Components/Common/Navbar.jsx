007e4262f78f899c5303fcf2b44d51ce257e2b94
thearsalan1 <arsalanloq@gmail.com>
2025-10-02 19:22:52 +0530

    implement cart UI and responsive hamburger menu

--- Frontend/src/Components/Common/Navbar.jsx
+++ Frontend/src/Components/Common/Navbar.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import {
   HiOutlineShoppingBag,
@@ -6,11 +6,24 @@ import {
   HiOutlineUser,
 } from "react-icons/hi2";
 import Searchbar from "./Searchbar";
+import CartDrawer from "../Layout/CartDrawer";
+import { IoMdClock, IoMdClose } from "react-icons/io";
 
 const Navbar = () => {
+  const [drawerOpen, setDrawerOpen] = useState(false);
+  const [navDrawerOpen, setNavDrawerOpen] = useState(false);
+
+  const toggleCartDrawer = () => {
+    setDrawerOpen(!drawerOpen);
+  };
+
+  const toggleHamBurger = () => {
+    setNavDrawerOpen(!navDrawerOpen);
+  };
+
   return (
     <>
-      <div className="container mx-auto flex items-center justify-between py-4 px-16">
+      <nav className="container mx-auto flex items-center justify-between py-4 ">
         <div>
           <Link to="/" className="text-2xl font-medium">
             Shopnosis
@@ -47,7 +60,10 @@ const Navbar = () => {
           <Link to="/profile" className="hover:text-black">
             <HiOutlineUser className="h-6 w-6 text-gray-700" />
           </Link>
-          <button className="relative hover:text-black">
+          <button
+            onClick={toggleCartDrawer}
+            className="relative hover:text-black"
+          >
             <HiOutlineShoppingBag className="h-6 w-6 text-gray-700" />
             <span className="absolute -top-1 bg-[#ea2e0e] text-white text-xs rounded-full px-2 py-0.5">
               4
@@ -55,9 +71,59 @@ const Navbar = () => {
           </button>
           <Searchbar />
 
-          <button className="md:hidden">
+          <button className="md:hidden" onClick={toggleHamBurger}>
             <HiBars3BottomRight className="h-6 w-6 text-gray-700" />
           </button>
+        </div>
+      </nav>
+      <CartDrawer toggleCartDrawer={toggleCartDrawer} drawerOpen={drawerOpen} />
+
+      <div
+        className={`fixed left-0 top-0 w-1/2 sm:w-2/5 md:w-1/5 h-full bg-white shadow-lg transform transition-transform duration-300 z-50 ${
+          navDrawerOpen ? "translate-x-0" : "-translate-x-full"
+        }`}
+      >
+        <div className="flex flex-col h-full ">
+          <div className="h-1/6 flex items-center justify-center border-b bg-gray-200">
+            Shopnosis
+            <button>
+              <IoMdClose
+                className="w-6 h-6 absolute right-2 top-2"
+                onClick={toggleHamBurger}
+              />
+            </button>
+          </div>
+          <h1 className="text-3xl font-semibold ml-6 mt-5">Menu</h1>
+          <div className="flex flex-col gap-5 px-6 pt-6">
+            <Link
+              to="#"
+              onClick={toggleHamBurger}
+              className="text-gray-700 hover:text-black text-sm font-medium uppercase"
+            >
+              Men
+            </Link>
+            <Link
+              to="#"
+              onClick={toggleHamBurger}
+              className="text-gray-700 hover:text-black text-sm font-medium uppercase"
+            >
+              Women
+            </Link>
+            <Link
+              to="#"
+              onClick={toggleHamBurger}
+              className="text-gray-700 hover:text-black text-sm font-medium uppercase"
+            >
+              Top Wear
+            </Link>
+            <Link
+              to="#"
+              onClick={toggleHamBurger}
+              className="text-gray-700 hover:text-black text-sm font-medium uppercase"
+            >
+              Bottom Wear
+            </Link>
+          </div>
         </div>
       </div>
     </>
---
+++ Frontend/src/Components/Layout/CartDrawer.jsx
@@ -0,0 +1,33 @@
+import { IoMdClose } from "react-icons/io";
+import CartContent from "./CartContent";
+
+const CartDrawer = ({ toggleCartDrawer, drawerOpen }) => {
+  return (
+    <div
+      className={`fixed top-0 right-0 w-full max-w-sm h-full bg-white shadow-lg transform transition-transform duration-300 flex flex-col z-50 ${
+        drawerOpen ? "translate-x-0" : "translate-x-full"
+      }`}
+    >
+      <div className="flex justify-end p-4">
+        <button onClick={toggleCartDrawer}>
+          <IoMdClose className="h-6 w-6 text-gray-600" />
+        </button>
+      </div>
+      <div className="flex flex-col grow p-4 overflow-y-auto">
+        <h2 className="text-xl font-semibold mb-4">Your Cart</h2>
+        <CartContent></CartContent>
+      </div>
+
+      <div className="sticky bottom-0 p-4">
+        <button className="  bg-red-500 w-full font-semibold text-white text-md py-3 rounded-lg">
+          Checkout
+        </button>
+        <p className="text-sm tracking-tighter text-gray-500 mt-2 text-center">
+          Your savings and tax will be factored in at the final step.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default CartDrawer;
