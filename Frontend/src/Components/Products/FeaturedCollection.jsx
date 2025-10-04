6914669a147d4defcd8a571c1801e3ac04715e9e
thearsalan1 <arsalanloq@gmail.com>
2025-10-04 14:44:58 +0530

    implemented collectuons ad banner and other features with responsive design

---
+++ Frontend/src/Components/Products/FeaturedCollection.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import featured from "../../assets/group.png";
+
+const FeaturedCollection = () => {
+  return (
+    <section className="py-16 px-4 lg:px-0">
+      <div className="contaienr mx-auto flex flex-col-reverse lg:flex-row items-center bg-blue-50 rounded-3xl">
+        {/* Left Content */}
+        <div className="lg:w-1/2 p-8 text-center lg:text-left">
+          <h2 className="text-lg font-semibold text-gray-700 mb-2">
+            🧥 Relaxed Fits, Refined Looks
+          </h2>
+          <h2 className="text-4xl lg:text-5xl font-bold mb-6">
+            Built for Real Life, Worn with Confidence
+          </h2>
+          <p className="text-lg text-gray-600 mb-6">
+            Crafted with care, styled with purpose. Because comfort should never
+            compromise style.
+          </p>
+          <Link
+            to="/collection/all"
+            className="bg-black text-white px-6 py-3 rounded-lg text-lg hover:text-gray-300"
+          >
+            Explore Now
+          </Link>
+        </div>
+        {/* Right Content */}
+        <div className="lg:w-1/2">
+          <img
+            src={featured}
+            alt="Featured Collection"
+            className="w-full h-full object-cover lg:rounded-tr-3xl lg:rounded-br-3xl"
+          />
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default FeaturedCollection;
--- Frontend/src/Components/Products/ProductGrid.jsx
+++ Frontend/src/Components/Products/ProductGrid.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 
 const ProductGrid = ({ products }) => {
   return (
-    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4">
+    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6 max-w-[90%] mx-auto">
       {products.map((product, index) => (
         <Link key={index} to={`/product/${product._id}`} className="block">
           <div className="bg-white p-4 rounded-lg">
